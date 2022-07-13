dd9957ff9113ea5f712df0c6a2e2e928055b313e
Juan Diego <juandiegomariscal@gmail.com>
2022-07-13 12:24:56 +0200

    feat: 👍creado metodo de borrado

--- balance/models.py
+++ balance/models.py
@@ -15,15 +15,7 @@ class DBManager:
         cursor.execute(consulta)
 
         #4 tratar los datos
-            #4.1 obtengo nombres de columna
-            # cursor.description devuelve (("nom_col", "val_col"))
-            #4.2 pedir todos los datos(registros)
-            #4.3 recorrer los resultados
-                #4.3.1 crear un diccionario
-                    #recorro la lista de los nombres de la columna
-                    # para cada columna: nombre+valor
-                #4.3.2 guardar en la lista de movimientos
-                # obtengo una lista: [{nombre1_col1:val_col1}, {nombre1_col2:val_col2}, ...]  
+
         self.movimientos = []
         nombres_columnas = []
 
@@ -40,5 +32,22 @@ class DBManager:
                 movimiento[nombre] = dato[indice]
                 indice += 1
             self.movimientos.append(movimiento)
+
+        conexion.close()
+        return self.movimientos
+
+    def borrar(self, id):
+        consulta = "DELETE from movimientos WHERE id=?"
+        conexion = sqlite3.connect(self.ruta)
+        cursor = conexion.cursor()
+        resultado = False
+        try:
+            cursor.execute(consulta, (id,)) #DELETE from movimientos WHERE id=3
+            conexion.commit()
+            resultado = True
+        except:
+            conexion.rollback()
+        conexion.close()   
+        return resultado
 
         
