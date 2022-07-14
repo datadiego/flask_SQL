6236fc17701ba02a8d63c679f164a30d895d1882
Juan Diego <juandiegomariscal@gmail.com>
2022-07-14 13:58:46 +0200

    feat: 👍validaciones creadas

--- balance/forms.py
+++ balance/forms.py
@@ -4,9 +4,14 @@ from wtforms.validators import DataRequired, Length
 class MovimientosForm(FlaskForm):
     id = HiddenField()
     fecha = DateField("Fecha", validators=[DataRequired("Debes introducir una fecha")])
-    concepto = StringField("Concepto", validators=[DataRequired()])
-    tipo = RadioField(choices=[("I", "Ingreso"), ("G", "Gasto")])
-    cantidad = FloatField("Cantidad")
+    concepto = StringField("Concepto", validators=[
+        DataRequired("Debes introducir un concepto"),
+        Length(3, 25, "Debe tener entre 3 y 25 caracteres")    
+    ])
+    tipo = RadioField(choices=[("I", "Ingreso"), ("G", "Gasto")],
+        validator=[DataRequired("¿Es un gasto o un tipo?")]
+    )
+    cantidad = FloatField("Cantidad", validators=[DataRequired("Debes escribir una cantidad")])
 
     submit = SubmitField("Guardar", render_kw={"class":"blue-button"})
 
